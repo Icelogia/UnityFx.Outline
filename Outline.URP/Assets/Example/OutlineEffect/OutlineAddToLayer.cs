520fd1d15b6860771da67ef9abe4495bba692702
Icelogia <35977372+Icelogia@users.noreply.github.com>
2025-11-14 21:00:27 +0100

    Examples

---
+++ Outline.URP/Assets/Example/OutlineEffect/OutlineAddToLayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityFx.Outline;
+
+public class OutlineAddToLayer : MonoBehaviour
+{
+	[SerializeField] private OutlineEffect outlineEffect;
+	[SerializeField] private int layerIndex = 0;
+
+	private void OnEnable()
+	{
+		outlineEffect.AddGameObject(gameObject, layerIndex);
+	}
+}
